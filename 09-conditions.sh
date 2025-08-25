d6650346d7d4aa73cfc13460f2e70b68abeabdfc
Trinath9395 <trinathdevops0606@gmail.com>
2025-08-25 12:02:03 +0530

    conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,7 +2,7 @@
 
 NUMBER=$1 
 
-if [$NUMBER -gt 100 ]; then 
+if [ $NUMBER -gt 100 ]; then 
   echo "Given number is greather than 100"
 else 
   echo "Given number is less than 100 or equal to 100"
