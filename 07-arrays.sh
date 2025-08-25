5f6c867be68cc412bbead2129fb13e896a6fd474
Trinath9395 <trinathdevops0606@gmail.com>
2025-08-25 11:04:00 +0530

    arrary types

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -5,3 +5,5 @@ MOVIES=("rrr" "devara" "dhee")
 echo "first movie name is: ${MOVIES[0]}"
 echo "second movie name is: ${MOVIES[1]}"
 echo "third movie name is: ${MOVIES[2]}"
+
+echo "all movies: ${MOVIES[*]}"
