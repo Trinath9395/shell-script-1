7d8ad7f1cc82b8e42feb6d1feb84274376282021
Trinath9395 <trinathdevops0606@gmail.com>
2025-08-25 09:34:46 +0530

    variables added

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSION1=DEV
+PERSION2=Stage
+
+echo "$PERSION1:: Hi $PERSION2"
+echo "$PERSION2:: Hello $PERSION1"
+echo "$PERSION1:: how are you $PERSION2"
+echo "$PERSION2:: im good $PERSION1"
