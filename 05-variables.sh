26741855442a738f5c2d538118024fbfd11c93c5
Trinath9395 <trinathdevops0606@gmail.com>
2025-08-25 09:57:38 +0530

    variables added

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,8 +1,9 @@
 #!/bin/bash 
 
 echo "Please enter your username:"
-echo "Please entet your password:"
-
 read -s USERNAME
+
+echo "Please entet your password:"
 read -s PASSWORD
+
 #echo "username entered: $USERNAME"
